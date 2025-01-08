cdc6d99c23e0252319c6f207871bff42be5df545
diegofa85 <diegofa85@educastur.es>
2025-01-08 21:34:27 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <div>Mujer</div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
