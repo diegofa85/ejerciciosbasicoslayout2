95b9a8c567ffdd1b499c9265979b5cfd3ab9a2ac
diegofa85 <diegofa85@educastur.es>
2025-01-10 19:04:00 +0100

    viernes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 
 function App() {
   return (
@@ -11,21 +12,15 @@ function App() {
           <div>Mujer</div>
         </Col>
         <Col md={4} xl={4}>
-          <div>Mujer</div>
+          <Image src={"./foto1.jpg"} style={{height:175, width:200, marginLeft: '200px'}}/>
         </Col>
         <Col md={4} xl={4}>
-          <div>Mujer</div>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4} xl={4}>
-          <div>Mujer</div>
-        </Col>
-        <Col md={4} xl={4}>
-          <div>Mujer</div>
-        </Col>
-        <Col md={4} xl={4}>
-          <div>Mujer</div>
+          <div style={{ marginTop: '25px' }}>
+            <p>David</p>
+            <p>Pérez</p>
+            <p>García</p>
+            <p>Foto de Carnet new</p>
+          </div>
         </Col>
       </Row>
       <Row>
@@ -33,10 +28,15 @@ function App() {
           <div>Mujer</div>
         </Col>
         <Col md={4} xl={4}>
-          <div>Mujer</div>
+          <Image src={"./foto1.jpg"} style={{height:175, width:200, marginLeft: '200px', marginTop: '50px'}}/>
         </Col>
         <Col md={4} xl={4}>
-          <div>Mujer</div>
+          <div style={{ marginTop: '25px' }}>
+            <p>David</p>
+            <p>Pérez</p>
+            <p>García</p>
+            <p>Foto de Carnet new</p>
+          </div>
         </Col>
       </Row>
       <Row>
@@ -44,10 +44,31 @@ function App() {
           <div>Mujer</div>
         </Col>
         <Col md={4} xl={4}>
-          <div>Mujer</div>
+          <Image src={"./foto1.jpg"} style={{height:175, width:200, marginLeft: '200px'}}/>
         </Col>
         <Col md={4} xl={4}>
-          <div>Mujer</div>
+          <div style={{ marginTop: '25px' }}>
+            <p>David</p>
+            <p>Pérez</p>
+            <p>García</p>
+            <p>Foto de Carnet new</p>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4} xl={4}>
+          <div style={{width:2000, marginTop: '150px'}}>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
+        </Col>
+        <Col md={4} xl={4}>
+          <Image src={"./foto1.jpg"} style={{height:175, width:200, marginLeft: '200px'}}/>
+        </Col>
+        <Col md={4} xl={4}>
+          <div style={{ marginTop: '25px' }}>
+            <p>David</p>
+            <p>Pérez</p>
+            <p>García</p>
+            <p>Foto de Carnet new</p>
+          </div>
         </Col>
       </Row>
     </Container>
